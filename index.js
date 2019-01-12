d6f51de8ccebf5996026707b1eae42f5ac496587
CoreZye <corezye@gmail.com>
2019-01-12 12:38:54 +0000

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-var proxy = require('http-proxy-middleware')
-
-var app = express()
-
-//app.use('/', proxy({ target: 'http://www.google.com', changeOrigin: true }))
-app.use('/', proxy({ target: 'http://www.ruv.is/', changeOrigin: true }))
-app.listen(80)
